5e27e0d4502348ac0620ed6cddad26cc352c86d9
you-knowww <jacob@youknowww.com>
2023-08-09 00:20:27 -0700

    very simple class with frame for getting token api

--- js/agocms.js
+++ js/agocms.js
@@ -1,5 +1,14 @@
+class Agocms {
+  async getToken(){
+    console.log(drupalSettings);
+    return();
+  }
+}
+
+
 (function (Drupal, drupalSettings){
-  console.log('pretest');
+ // console.log('pretest');
+  /*
   arcgisRest
     .getSelf(
       { hideToken: true,
@@ -13,4 +22,8 @@
         hideToken: true,
         params: { token: drupalSettings.ago_access_token.access_token }})
     .then(response => console.log(response));
+    */
+  const agocms = new Agocms;
+
+  agocms.getToken().then(result => console.log(result));
 })(Drupal, drupalSettings);
