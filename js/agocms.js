cba42f95c8176c14686fba9b5fb646307b3e07d2
you-knowww <jacob@youknowww.com>
2023-08-15 00:41:25 -0700

    token management with broadastChannel to maintain for login across windows/tabs. next step is to actually refresh token after ago social auth updeates to provide base url

--- js/agocms.js
+++ js/agocms.js
@@ -1,31 +1,50 @@
 class Agocms {
   // default locked to false but check lock on init
   #locked = false;
+  // objects
   #access;
   #refresh;
+  // ago username
   #user;
+  // manage events across context in origin. const
+  #ch = new BroadcastChannel("agocms");
+  // local event listener to dispatch after updating local creds. const
+  #ev_updatedToken = new Event('local_token_updated');
 
   constructor(){
     // context
     const agocms = this;
 
     // set event listener for lock and unlock
-    window.addEventListener('agocms_lock_token', () => agocms.locked = true);
-    window.setEventListener('agocms_unlock_token', u => agocms.updateTokenRefs(u));
+    this.#ch.onmessage = e => {
+          // ref
+          const data = message.data;
 
-    //
+          switch(data.event){
+            case 'lock':
+              // lock down requests
+              agocms.#locked = true;
+              break;
+            case 'unlock':
+              // begin unlocking. first need to update refs
+              agocms.#updateTokenRefs(data.token_data);
+          } };
+
     // init shorthand variables
-    this.access = { token: drupalSettings.hasOwnProperty('ago_access_token')
-                            ? drupalSettings.hasOwnProperty.access_token : undefined,
+    this.#access = {token: drupalSettings.hasOwnProperty('ago_access_token')
+                            ? drupalSettings.ago_access_token.access_token
+                            : undefined,
                     expiration: drupalSettings.hasOwnProperty('ago_access_token')
-                                ? drupalSettings.hasOwnProperty.expires : undefined };
-    this.refresh = {token: drupalSettings.hasOwnProperty('ago_access_token')
-                            ? drupalSettings.hasOwnProperty.refresh_token : undefined,
-                    expiration: drupalSettings.hasOwnProperty('ago_access_token')
-                                ? drupalSettings.hasOwnProperty.refresh_token_expires_in
+                                ? drupalSettings.ago_access_token.expires
                                 : undefined };
-    this.user = drupalSettings.hasOwnProperty('ago_access_token')
-                ? drupalSettings.hasOwnProperty.username : undefined;
+    this.#refresh = { token: drupalSettings.hasOwnProperty('ago_access_token')
+                              ? drupalSettings.ago_access_token.refresh_token
+                              : undefined,
+                    expiration: drupalSettings.hasOwnProperty('ago_access_token')
+                                ? drupalSettings.ago_access_token.refresh_token_expires_in
+                                : undefined };
+    this.#user = drupalSettings.hasOwnProperty('ago_access_token')
+                  ? drupalSettings.ago_access_token.username : undefined;
   }
 
   // async return access token as string
@@ -36,18 +55,18 @@ class Agocms {
     // async
     return new Promise((resolve, reject) => {
       // check lock
-      if(agocms.locked === true){
+      if(agocms.#locked === true){
         // set event listener on unlock
         window.setEventListener('local_token_updated',
-            () => resolve(agocms.access.token));
+            () => resolve(agocms.#access.token));
       } else {
         // check token expiration
-        if(agocms.isAccessTokenExpired()){
+        if(agocms.#isAccessTokenExpired()){
           // get new token
-          agocms.refreshToken().then(() => resolve(agocms.access.token));
+          agocms.#refreshToken().then(() => resolve(agocms.#access.token));
         } else {
           // get the token we have
-          resolve(agocms.access.token);
+          resolve(agocms.#access.token);
         }
       }
     });
@@ -61,33 +80,39 @@ class Agocms {
     // https://stackoverflow.com/questions/3367415/get-epoch-for-a-specific-date-using-javascript
     const nowAgoFmt = new Date().getTime() / 1000;
     // allow 10 seconds between expiration and renewal
-    return nowAgoFmt + 10 >= agocms.access.expiration;
+    return nowAgoFmt + 10 >= agocms.#access.expiration;
   }
 
   #refreshToken() {
+    // context
+    const agocms = this;
+
     // async
     return new Promise((resolve, reject) => {
-      // lock and broadast lock to any other open tabs
-      agocms.broacastLock();
-
-      // get new token from
+      // lock and broadcast lock to any other open tabs
+      agocms.#ch.postMessage({event: 'lock'});
+      /*
+      // get new token from ago
+      arcgisRest.fetchToken(url, {})
+        .then(response => {
+          agocms.#ch.postMessage({event: 'unlock', token_data: response});
+        })
+      */
     });
-  }
-
-  #broadcastLock(){
-
   }
 
   #updateTokenRefs(tokenData) {
     // init shorthand variables
-    this.access.token = tokenData.access_token;
-    this.access.expiration: tokenData.access_expiration;
-    this.refresh.token = tokenData.refresh_token;
-    this.refresh.expiration: tokenData.refresh_expires;
+    this.#access.token = tokenData.access_token;
+    this.#access.expiration = tokenData.access_expiration;
+    this.#refresh.token = tokenData.refresh_token;
+    this.#refresh.expiration = tokenData.refresh_expires;
 
-    // fire window event
-    const ev_updatedToken = new Event('local_token_updated');
-    window.fire(ev_updatedToken);
+    // unlock
+    this.#locked = false;
+
+    // dispatch window event
+    window.dispatchEvent(this.#ev_updatedToken);
   }
 }
 
@@ -110,6 +135,7 @@ class Agocms {
   */
 
   const agocms = new Agocms;
-
-  agocms.getToken().then(result => console.log(result));
+  console.log(drupalSettings);
+  console.log(agocms);
+  //agocms.getToken().then(result => console.log(result));
 })(Drupal, drupalSettings);
