d5f547ed935cb49943a2a6a02fb86112894151f9
yk <jacobmidtlyng@gmail.com>
2023-10-26 23:21:28 -0700

    Service select tool in config

--- assets/custom/js/view-conf.js
+++ assets/custom/js/view-conf.js
@@ -1,4 +1,4 @@
-function agocmsViewConfigFormGroupSearch(searchText){
+function agocmsViewConfigFormGroupSearch(searchText = ''){
   return new Promise((resolve, reject) => {
     // validate search text
     if(searchText == '') resolve([]);
@@ -9,10 +9,33 @@ function agocmsViewConfigFormGroupSearch(searchText){
 
     // validate token and search. return array. Empty on failure
     agocms.ajx(arcgisRest.searchGroups, {q, sortField: 'title'})
-      .then(response => {
-          // validate respond with array of results or empty array
-          resolve(Array.isArray(response.results) ? response.results : []);
-        },
+      .then(response =>
+          resolve(Array.isArray(response.results) ? response.results : []),
         () => resolve([]));
   });
+}
+
+function agocmsViewConfigFormServiceSearch(searchText = '', groupId = ''){
+  return new Promise((resolve, reject) => {
+    // validate search text
+    if(searchText == '' && groupId == '') resolve([]);
+
+    // keyword search on all items filtered to Feature Services
+    const q = new arcgisRest.SearchQueryBuilder()
+                .match('Feature Service').in('type');
+
+    // skip if search is empty
+    if(searchText !== '') q.and().match(searchText).in('title');
+
+    // if no group set, search all available feature services
+    const searchFn = groupId == ''
+                      ? arcgisRest.searchItems
+                      : arcgisRest.searchGroupContent;
+
+    // run group search if groupId is set. If not, groupId ignored by api
+    agocms.ajx(searchFn, {q, groupId, sortField: 'title'})
+      .then(response =>
+          resolve(Array.isArray(response.results) ? response.results : []),
+        () => resolve([]));
+  })
 }
