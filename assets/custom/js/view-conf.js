948f7dab2d7a0528b071fd9e3be929435444e348
yk <jacobmidtlyng@gmail.com>
2023-11-01 23:43:42 -0700

    begin using template els and connect to hyperscript. begin forming field templates

--- assets/custom/js/view-conf.js
+++ assets/custom/js/view-conf.js
@@ -1,3 +1,32 @@
+function agocmsViewConfigFormAddLayer() {
+  // get template and container
+  const tpl_layerForm = document.getElementById('agocmsFeatureLayerSelect'),
+        el_layerContainer = document.getElementById('agocmsViewMapConfLayers');
+
+  // get existing list count. clone form as element
+  const layerCnt = el_layerContainer.children.length,
+        el_layerForm = document.importNode(tpl_layerForm.content, true);
+
+  // find datalist and their input to update ID and ref to layer specific
+  el_layerForm.querySelectorAll('datalist').forEach(el_datalist => {
+    // ref input for datalist
+    const el_input = el_layerForm.querySelector('input[list="'+ el_datalist.id +'"');
+
+    // update datalist id to unique val and ref in list attrib
+    el_datalist.id = el_datalist.id + layerCnt;
+    el_input.setAttribute('list', el_datalist.id);
+  })
+
+  // make hyperscript el refs before appending to container
+  const els_hypserscripters = el_layerForm.querySelectorAll('[\_]');
+
+  // get container and add template content to it. wonder if this works with hypertext
+  el_layerContainer.appendChild(el_layerForm);
+
+  // apply hyperscript on necessary els. the fun times.
+  els_hypserscripters.forEach(el => _hyperscript.processNode(el));
+}
+
 function agocmsViewConfigFormGroupSearch(searchText = ''){
   return new Promise((resolve, reject) => {
     // validate search text
@@ -38,4 +67,24 @@ function agocmsViewConfigFormServiceSearch(searchText = '', groupId = ''){
           resolve(Array.isArray(response.results) ? response.results : []),
         () => resolve([]));
   })
+}
+
+function agocmsViewConfigFormLayerSearch(url) {
+  return new Promise((resolve, reject) => {
+    // validate
+    if(url == '') resolve([]);
+
+    // get layers in feature service
+    agocms.ajx(arcgisRest.getAllLayersAndTables, {url})
+      .then(response => {
+        // validate
+        if(response.hasOwnProperty('error')){
+          console.error('FAILURE: service layers.', response.error);
+          resolve([]);
+        } else {
+          // convert array with name property to add to select label
+          resolve(Object.entries(response).map(([key, group]) => { return {name: key, layers: group}; }));
+        }
+      });
+  });
 }
